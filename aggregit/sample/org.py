48fba605fa7e71b1ef80e19c724b1c9516b89052
MichaelCurrin <michael.currin@gmail.com>
2018-09-05 23:11:49 +0200

    feature: Update org.py

--- aggregit/sample/org.py
+++ aggregit/sample/org.py
@@ -2,34 +2,27 @@
 Sample organization module.
 """
 from etc import config
-from lib.connection import CONN
 
 
-o = CONN.get_organization(config.ORGANIZATION)
+def print_details(org):
+    events = list(org.get_events())
+    members = list(org.get_members())
+    issues = list(org.get_issues())
+    repos = list(org.get_repos())
 
-events = list(o.get_events())
-members = list(o.get_members())
-issues = list(o.get_issues())
-repos = list(o.get_repos())
-print('Events: {}'.format(len(events)))
-print('Members: {}'.format(len(members)))
-print('Issues: {}'.format(len(issues)))
-print('Repos: {}'.format(len(repos)))
+    print("Name: {}".format(org.name))
+    print("Events: {}".format(len(events)))
+    print("Members: {}".format(len(members)))
+    print("Issues: {}".format(len(issues)))
+    print("Repos: {}".format(len(repos)))
+    print()
 
-teams = o.get_teams()
 
-print("All teams and member count")
-for t in teams:
-    print(t.name, t.members_count)
-print()
+def main():
+    from lib.connection import CONN
+    org = CONN.get_organization(config.ORGANIZATION)
+    print_details(org)
 
-print("Actual members for configured teams")
-for t in teams:
-    if t.name in config.TEAMS:
-        print(t.name)
-        members = [("@{}".format(u.login), u.name if u.name else "N/A",
-                    u.company if u.company else "N/A")
-                   for u in t.get_members()]
-        for m in members:
-            print(*m)
-        print()
+
+if __name__ == '__main__':
+    main()
