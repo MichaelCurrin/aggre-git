1586478f3a24fd413c6488c28266a6fc476bd2fa
MichaelCurrin <michael.currin@gmail.com>
2018-09-08 11:21:53 +0200

    feature: Create sample issues scripts

---
+++ aggregit/sample/repos/issues/comments.py
@@ -0,0 +1,21 @@
+"""
+Sample repo issue comments module.
+"""
+from etc import config
+from lib.connection import CONN
+
+
+def main():
+    for repo_name in config.REPOS:
+        repo = CONN.get_repo(repo_name)
+        print(repo.name)
+
+        for comment in repo.get_issues_comments():
+            data = {'username': comment.user.login,
+                    'date': str(comment.created_at.date())}
+            print(data)
+        print()
+
+
+if __name__ == '__main__':
+    main()
