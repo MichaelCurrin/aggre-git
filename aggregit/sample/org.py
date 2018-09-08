af3eea80ce2de5e68c054c6cdc66c591a47125d2
MichaelCurrin <michael.currin@gmail.com>
2018-09-08 11:21:35 +0200

    refactor: Update org.py

--- aggregit/sample/org.py
+++ aggregit/sample/org.py
@@ -4,23 +4,25 @@ Sample organization module.
 from etc import config
 
 
-def print_details(org):
-    events = list(org.get_events())
-    members = list(org.get_members())
-    issues = list(org.get_issues())
-    repos = list(org.get_repos())
+def extract(org):
+    details = dict(
+        name=org.name,
+        login=org.login,
+        events=len(list(org.get_events())),
+        members=len(list(org.get_members())),
+        issues=len(list(org.get_issues())),
+        repos=len(list(org.get_repos())),
+        teams=len(list(org.get_teams()))
+    )
 
-    print("Name: {}".format(org.name))
-    print("Events: {}".format(len(events)))
-    print("Members: {}".format(len(members)))
-    print("Issues: {}".format(len(issues)))
-    print("Repos: {}".format(len(repos)))
+    return details
 
 
 def main():
     from lib.connection import CONN
     org = CONN.get_organization(config.ORGANIZATION)
-    print_details(org)
+    details = extract(org)
+    print(details)
 
 
 if __name__ == '__main__':
