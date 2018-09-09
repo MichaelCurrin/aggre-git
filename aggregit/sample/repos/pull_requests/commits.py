6f3c6d9a82a228ccc9fc568abc851c7230fef552
MichaelCurrin <michael.currin@gmail.com>
2018-09-09 17:04:06 +0200

    feature: Create pull_requests/commits.py

---
+++ aggregit/sample/repos/pull_requests/commits.py
@@ -0,0 +1,37 @@
+"""
+Sample pull request commits module.
+"""
+import pprint
+
+from etc import config
+import lib
+from lib.connection import CONN
+
+
+def extract(commit):
+    date = lib.parse_commit_date(commit.stats.last_modified).date()
+
+    return dict(
+        author=commit.author.login if commit.author else "N/A",
+        SHA=commit.sha,
+        last_modified=str(date),
+        additions=commit.stats.additions,
+        deletions=commit.stats.deletions,
+        total=commit.stats.total
+    )
+
+
+def main():
+    repos = [CONN.get_repo(repo_name) for repo_name in config.REPOS]
+
+    for repo in repos:
+        print(repo.name)
+        for commit in repo.get_commits():
+            data = extract(commit)
+            pprint.pprint(data)
+            print()
+        print()
+
+
+if __name__ == '__main__':
+    main()
