7c23693b3a6f5f326532616bdba9c74828826875
MichaelCurrin <michael.currin@gmail.com>
2018-09-09 17:25:16 +0200

    refactor: Udpate connection.py

--- aggregit/lib/connection.py
+++ aggregit/lib/connection.py
@@ -1,9 +1,12 @@
 """
 Connection module.
-"""
-from etc import config
 
+Note that username and password can be used as arguments instead of token, but
+you can error if your account is setup for security to use OTP.
+"""
 from github import Github
+
+from etc import config
 
 
 CONN = Github(config.ACCESS_TOKEN)
