2804e1f8a801592e1ec93684f478f321b30251a6
MichaelCurrin <michael.currin@gmail.com>
2018-09-24 22:59:41 +0200

    feature: Add error catching to teams.py

--- aggregit/sample/teams.py
+++ aggregit/sample/teams.py
@@ -1,18 +1,28 @@
 """
 Sample teams module.
 """
-from . import user
+from github import UnknownObjectException
+
+from sample import user
 from etc import config
 from lib.connection import CONN
 
 
 def main():
-    o = CONN.get_organization(config.REPO_OWNER)
-    teams = o.get_teams()
+    try:
+        o = CONN.get_organization(config.REPO_OWNER)
+    except UnknownObjectException as e:
+        e.data['message'] = f"Could not finder organization: {config.REPO_OWNER}"
+        raise
+
+    teams = list(o.get_teams())
+
+    print(f"Teams: {len(teams)}\n")
 
     for t in teams:
         print(f"Team: {t.name}")
         print("---")
+
         for m in t.get_members():
             user.print_details(m)
             print()
