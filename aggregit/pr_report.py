55e2104701b70466287cc5b4b4bb04b3c236af2a
Michael Currin <mcurrin@2u.com>
2018-10-07 20:47:14 +0200

    feature: Write out CSV using pr_report.py

--- aggregit/pr_report.py
+++ aggregit/pr_report.py
@@ -11,9 +11,13 @@ are included.
 A commit doesn't have to have an author - if blank assume it was by the PR
 author as it probably was.
 """
+import csv
+
 from etc import config
 from lib.connection import CONN
+from models import PullRequest
 
+out_data = []
 
 if config.BY_OWNER:
     user = CONN.get_user(config.REPO_OWNER)
@@ -26,9 +30,29 @@ else:
     repos = [CONN.get_repo(repo_path) for repo_path in config.REPO_PATHS]
 
 for repo in repos:
-    print(repo.name)
+    print(f"REPO: {repo.name}")
     for pr in repo.get_pulls(state=config.PR_STATE):
-        author = pr.user
-        print(author.login)
-        for commit in pr.get_commits():
-            print(commit)
+        author_login = pr.user.login
+
+        if author_login in config.USERNAMES:
+            print(f"PR #{pr.number}")
+            pr_data = PullRequest(pr)
+
+            out_row = {
+                'owner': repo.owner.login,
+                'repo': repo.name,
+                'pr_author': author_login,
+                'pr_assignees': ", ".join(pr_data.assignee_logins()),
+                'pr_date': str(pr_data.created_at),
+                'pr_no': pr_data.number,
+                'pr_title': pr_data.title,
+                'pr_status': pr_data.status
+            }
+            out_data.append(out_row)
+
+header = ('owner', 'repo', 'pr_author', 'pr_assignees', 'pr_date', 'pr_no',
+          'pr_title', 'pr_status')
+with open(config.PR_CSV_PATH, 'w') as f_out:
+    writer = csv.DictWriter(f_out, fieldnames=header)
+    writer.writeheader()
+    writer.writerows(out_data)
