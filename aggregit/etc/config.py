289a9aa62cc3405da5a7c02850c871db65e63901
Michael Currin <mcurrin@2u.com>
2018-10-07 13:58:32 +0200

    refactor: Update config files

--- aggregit/etc/config.py
+++ aggregit/etc/config.py
@@ -8,8 +8,7 @@ try:
 except ImportError:
     import os
     f_path = os.path.join(os.path.dirname(__file__), 'configlocal.py')
-    raise ImportError("You need to create the missing file"
-                      " at {}".format(f_path))
+    raise ImportError(f"You need to create a local config file at {f_path}")
 
 assert ACCESS_TOKEN, "Please set the ACCESS_TOKEN value in the local config" \
                      " file"
