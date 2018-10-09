a0d68cd5181ac8dda02a7489ea9f47e0fb2f41d6
Michael Currin <michael.currin@gmail.com>
2018-10-09 19:54:21 +0200

    feature: Create Review in models.py refactor: Update PullRequest class

--- aggregit/models.py
+++ aggregit/models.py
@@ -14,7 +14,7 @@ import lib
 
 class Commit:
     """
-    Model a Git commit with just data of interest.
+    Model a Git commit, with just data of interest.
 
     Expects a PyGithub Commit object as returned from the API.
     """
@@ -28,22 +28,79 @@ class Commit:
         self.total = commit.stats.total
 
 
+class Review:
+    """
+    Model a Git pull request review, with just data of interest.
+
+    Expects a PyGithub Commit object as returned from the API.
+    Review stats must be one of:
+        'COMMENTED' 'APPROVED' 'DISMISSED' 'CHANGES_REQUESTED'
+    """
+
+    def __init__(self, review: github.PullRequestReview.PullRequestReview):
+
+        self.state = review.state
+        self.submitted_at = review.submitted_at.date()
+        self.reviewer = review.user
+
+    def reviewer_login(self):
+        return self.reviewer.login
+
+    def summary(self):
+        return f"{str(self.submitted_at)}:{self.reviewer_login()}:{self.state}"
+
+
 class PullRequest:
     """
-    Model a Git pull request with just data of interest.
+    Model a Git pull request,with just data of interest.
 
     Expects a PyGithub Pull Request object as returned from the API.
     """
 
-    def __init__(self, pull_request: github.PullRequest.PullRequest):
-        self.number = pull_request.number
-        self.title = pull_request.title
-        self.status = pull_request.state
-        self.created_at = pull_request.created_at.date()
+    def __init__(self, pr: github.PullRequest.PullRequest):
+        self.number = pr.number
+        self.title = pr.title
+        self.author = pr.user.login
+
+        self.state = pr.state
+        self.merged = pr.merged
+        if pr.merged:
+            self.merged_at = pr.merged_at.date()
+            self.merged_by = pr.merged_by.login
+        else:
+            self.merged_at = None
+            self.merged_by = None
+
+        self.created_at = pr.created_at.date()
+        self.updated_at = pr.updated_at.date()
+
         # Counts across all users, not just the author.
-        self.commit_count = pull_request.commits
-        self.changed_files = pull_request.changed_files
-        self.assignees = pull_request.assignees
+        self.commit_count = pr.commits
+
+        self.changed_files = pr.changed_files
+
+        # This is optional but not sure if this is assigned to do the work or
+        # review, as its separate to reviewers. This comes as a list not a
+        # paginated list.
+        self.assignees = pr.assignees
+
+        # TODO: Add methods to get dates and results of reviews.
+        self.reviews = [Review(review) for review in pr.get_reviews()]
+
+    def status(self):
+        if self.state == 'closed':
+            return 'CLOSED'
+
+        if self.merged:
+            return 'MERGED'
+        else:
+            return 'OPEN'
 
     def assignee_logins(self):
         return sorted([user.login for user in self.assignees])
+
+    def reviewer_logins(self):
+        return sorted([review.reviewer_login() for review in self.reviews])
+
+    def review_summary(self):
+        return [review.summary() for review in self.reviews]
