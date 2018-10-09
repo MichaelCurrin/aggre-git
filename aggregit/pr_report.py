e587842d2c535c6511af2bd54c0f2af30027fd2b
Michael Currin <michael.currin@gmail.com>
2018-10-09 20:32:33 +0200

    feature: Add fields to PR report

--- aggregit/pr_report.py
+++ aggregit/pr_report.py
@@ -40,22 +40,36 @@ for repo in repos:
             out_row = {
                 'repo_owner': repo.owner.login,
                 'repo': repo.name,
+
                 'author': author_login,
                 'assignees': ", ".join(pr_data.assignee_logins()),
+
                 'no': pr_data.number,
-                'date': str(pr_data.created_at),
                 'title': pr_data.title,
+
+                'created_at': str(pr_data.created_at),
+                'updated_at': str(pr_data.updated_at),
                 'status': pr_data.status(),
+                'merged_or_closed_at': str(pr_data.merged_or_closed_date()),
+
                 'reviewers': ", ".join(pr_data.reviewer_logins()),
-                'reviews': ", ".join(pr_data.review_summary())
+                'reviews': ", ".join(pr_data.review_summary()),
+
+                'commits': pr_data.commit_count,
+                'comments': pr_data.comment_count,
+                'changed_files': pr_data.changed_files,
+                'additions': pr_data.additions,
+                'deletions': pr_data.deletions,
             }
             out_data.append(out_row)
 
 header = (
     'repo_owner', 'repo',
     'author', 'assignees',
-    'no', 'date', 'title', 'status',
+    'no', 'title',
+    'created_at', 'updated_at', 'status', 'merged_or_closed_at',
     'reviewers', 'reviews',
+    'commits', 'comments', 'changed_files', 'additions', 'deletions',
 )
 with open(config.PR_CSV_PATH, 'w') as f_out:
     writer = csv.DictWriter(f_out, fieldnames=header)
