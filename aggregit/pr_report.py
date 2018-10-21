f58ca22332def28065f286a4f7e2691220ad84ec
Michael Currin <michael.currin@gmail.com>
2018-10-21 19:45:44 +0200

    feature: Make PR report more robust

--- aggregit/pr_report.py
+++ aggregit/pr_report.py
@@ -32,41 +32,48 @@ else:
 
 for repo in repos:
     print(f"REPO: {repo.name}")
+
     for pr in repo.get_pulls(state=config.PR_STATE):
         author = pr.user
 
         if author.login in config.USERNAMES:
             print(f"PR #{pr.number}")
-            pr_data = PullRequest(pr)
-            out_row = {
-                'Repo URL': repo.html_url,
-                'Repo Owner': lib.display(repo.owner),
-                'Repo Name': repo.name,
+            try:
+                pr_data = PullRequest(pr)
+                out_row = {
+                    'Repo URL': repo.html_url,
+                    'Repo Owner': lib.display(repo.owner),
+                    'Repo Name': repo.name,
 
-                'Author': lib.display(author),
-                'PR ID': f"#{pr_data.number}",
-                'PR Title': pr_data.title,
-                'PR URL': pr_data.url,
+                    'Author': lib.display(author),
+                    'PR ID': f"#{pr_data.number}",
+                    'PR Title': pr_data.title,
+                    'PR URL': pr_data.url,
 
-                'Created At': pr_data.created_at,
-                'Updated At': pr_data.updated_at,
+                    'Created At': pr_data.created_at,
+                    'Updated At': pr_data.updated_at,
 
-                'Status': pr_data.status,
+                    'Status': pr_data.status,
 
-                'Status Changed At': pr_data.status_changed_at(),
-                'Merged By': pr_data.merged_by_name(),
+                    'Status Changed At': pr_data.status_changed_at(),
+                    'Merged By': pr_data.merged_by_name(),
 
-                'Reviewers': ", ".join(pr_data.reviewer_names()),
-                'Reviews': ", ".join(pr_data.review_summary()),
+                    'Reviewers': ", ".join(pr_data.reviewer_names()),
+                    'Reviews': ", ".join(pr_data.review_summary()),
 
-                'Comments': pr_data.comment_count,
-                'Commits': pr_data.commit_count,
-                'Changed Files': pr_data.changed_files,
-                'Added Lines': pr_data.additions,
-                'Deleted Lines': pr_data.deletions,
-                'Changed Lines': pr_data.additions + pr.deletions,
-            }
-            out_data.append(out_row)
+                    'Comments': pr_data.comment_count,
+                    'Commits': pr_data.commit_count,
+                    'Changed Files': pr_data.changed_files,
+                    'Added Lines': pr_data.additions,
+                    'Deleted Lines': pr_data.deletions,
+                    'Changed Lines': pr_data.additions + pr.deletions,
+                }
+                out_data.append(out_row)
+            except Exception as e:
+                # Keep the report generation robust by logging and skipping over
+                # any errors.
+                print(f"Could not fetch or parse PR."
+                      f" {type(e).__name__}: {str(e)}")
 
 header = (
     'Repo Owner', 'Repo Name', 'Repo URL',
