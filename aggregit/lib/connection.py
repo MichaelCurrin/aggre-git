b46ca4bec20a6a1e8db630c9ee9fbb347a9ee0c2
Michael Currin <michael.currin@gmail.com>
2019-01-18 17:41:04 +0200

    docs: Update connection.py docstring

--- aggregit/lib/connection.py
+++ aggregit/lib/connection.py
@@ -8,6 +8,13 @@ Usage:
 Note that this script does not actually validate credentials or connect to
 Github. If you do a request using bad credentials, you will get
 a `github.BadCredentialsException` error.
+
+According to the PyGithub script where the Github class is defined, the
+global timeout is set at 15 seconds. That is already long so don't bother
+extending. Also, the there is a note in the script.
+    As of 2018-05-17, Github imposes a 10s limit for completion of API requests.
+    Thus, the timeout should be slightly > 10s to account for network/front-end
+    latency.
 """
 from github import Github
 
